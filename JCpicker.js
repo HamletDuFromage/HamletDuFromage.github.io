5e6c7334e4d73e1bc1d2f2f785918861e6ced758
flb <f.lb@gmx.com>
2020-03-29 16:52:39 +0200

    first commit

---
+++ JCpicker.js
@@ -0,0 +1,42 @@
+function download(file){
+    var obj = document.getElementById("json_output").value;
+    obj = obj.replace(/\n/g, "%0D%0A"); 
+    var data = "text/json;charset=utf-8," + obj;
+
+    var a = document.createElement('a');
+    a.href = 'data:' + data;
+    a.download = file;
+
+    a.style.display = 'none';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    
+}
+
+document.getElementById("dl").onclick = function() { 
+    download("profiles.json"); 
+};
+
+document.getElementById("gen").onclick = function() { 
+    var name = document.getElementById("profile_name").value;
+    if(name === ""){
+        name = "Custom Profile";
+    }
+    var profile = {
+        "name": name,
+        "L_JC" : document.getElementById("ljc_color").value,
+        "L_BTN" : document.getElementById("dpad_color").value,
+        "R_JC" : document.getElementById("rjc_color").value,
+        "R_BTN" : document.getElementById("buttons_color").value
+    }
+
+    var data = JSON.stringify(profile, null, 4);
+    if(document.getElementById("json_output").value === ""){
+        document.getElementById("json_output").value += data;
+    }
+    else{
+        document.getElementById("json_output").value += ",\n\n" + data;
+    }
+
+};
